02736f3ea9ea95a3c2a0565863dda4fddcceaf1b
Eric McCullough <eric96@live.missouristate.edu>
2020-10-12 19:56:31 -0500

    Implement proof of concept tutorial

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def root_controller():
+    with open('./static/js/WebGL_squary_eg.js') as fin:
+        starter_code = fin.read()
+
     return render_template(
-        'index.html'
+        'index.html',
+        starter_code = starter_code
     )
 
 
